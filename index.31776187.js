fb057a0574a2be9c55e99c98e803ae1a2ca48176
Vitaliy <vitaliklevytskyi07@gmail.com>
2024-09-12 17:10:23 +0300

    make build

---
+++ index.31776187.js
@@ -0,0 +1,2 @@
+Array.from(document.querySelector("table").querySelectorAll("tr")).forEach(function(e){var r=e.children[1];if(r){var l=r.cloneNode(!0);e.insertBefore(l,e.children[e.children.length-1])}});
+//# sourceMappingURL=index.31776187.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const table = document.querySelector("table");
+function cloneAndInsertColumn() {
+    const rows = Array.from(table.querySelectorAll("tr"));
+    rows.forEach((row)=>{
+        const secondCell = row.children[1];
+        if (secondCell) {
+            const clonedCell = secondCell.cloneNode(true);
+            row.insertBefore(clonedCell, row.children[row.children.length - 1]);
+        }
+    });
+}
+cloneAndInsertColumn();
+
+//# sourceMappingURL=index.f75de5e1.js.map
